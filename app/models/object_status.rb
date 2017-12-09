09d7ca81fe910b9bfce1e8bf28c0a818cb103ae5
kanmani <kanmaniselvan@gmail.com>
2017-12-09 23:01:53 +0530

    Made bulk import to happen per 1000 records
    * Handle condition if empty JSON is passed.
    * Remove Jquery form submit and make rails way of submit.

--- app/models/object_status.rb
+++ app/models/object_status.rb
@@ -3,7 +3,7 @@ class ObjectStatus < ApplicationRecord
   # Skipping all model validations since there is DB validation.
   # And, adding model validations will slow down bulk insert.
 
-  IMPORT_STATUS_BROADCAST_THRESHOLD = 1000
+  NUM_MAX_IMPORT = 1000
 
   def self.perform_import(file)
     object_status = []
@@ -17,21 +17,20 @@ class ObjectStatus < ApplicationRecord
       object_status << ObjectStatus.new(object_id: row[0],
                                         object_type: row[1],
                                         timestamp: row[2],
-                                        object_changes: JSON.parse(row[3]))
+                                        object_changes: JSON.parse(row[3].to_s))
 
 
-      # Update the status for each 1000 records
-      if IMPORT_STATUS_BROADCAST_THRESHOLD == row_index || total_rows_count == $.
-        broadcast_import_status("Parsing the CSV file ... #{($..to_f * 100/total_rows_count).round(1)}% completed")
+      # Import and Update the status for each NUM_MAX_IMPORT records
+      if NUM_MAX_IMPORT == row_index || total_rows_count == $.
+        broadcast_import_status("Importing the CSV file ... #{($..to_f * 100/total_rows_count).round(1)}% completed")
+
+        # Finally import everything in bulk.
+        ObjectStatus.import object_status, on_duplicate_key_ignore: true,  validate: false
 
         row_index = 0
+        object_status = []
       end
     end
-
-    broadcast_import_status('Importing records into the Database ...')
-
-    # Finally import everything in bulk.
-    ObjectStatus.import object_status, on_duplicate_key_ignore: true,  validate: false
 
     broadcast_import_status('Import Successful')
   end
